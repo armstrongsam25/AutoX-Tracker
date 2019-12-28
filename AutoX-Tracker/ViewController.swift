67b90bfdda7ed2fd4b62acdabe8e566c118ce9a2
Sam Armstrong <armstrongsam25@gmail.com>
2019-12-28 00:21:36 -0500

    Initial

---
+++ AutoX-Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoX Tracker
+//
+//  Created by Sam Armstrong on 12/28/19.
+//  Copyright © 2019 Samuel Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
